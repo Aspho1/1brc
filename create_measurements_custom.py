433c175aab6d154124367843a0bc491c171adff8
Aspho1 <thl3141@gmail.com>
2026-05-29 12:47:56 -0500

    multiprocessing the create_measurements code
    Signed-off-by: Aspho1 <thl3141@gmail.com>

--- create_measurements_custom.py
+++ create_measurements_custom.py
@@ -1,44 +1,50 @@
+import cProfile
 import random
+from multiprocessing import Pool
 
 COLDEST_TEMP = -99.9
 HOTTEST_TEMP =  99.9
 
-def main():
-    locations = get_locations()
-    write_new_measurements(locations)
+def get_locations() -> list[bytes]:
+    locations:list[bytes] = []
 
-
-def get_locations():
-    locations = []
-
-    with open('weather_stations.csv', 'r', encoding='utf-8') as f:
-        f.readline()
-        f.readline()
-        while True:
-            try:
-                loc, _ = f.__next__().strip('\n').split(';',1)
-                locations.append(loc)
-            except StopIteration:
-                break
-        
+    with open('weather_stations.csv', 'r+b') as f:
+        for line in f.readlines(): #b'\n'
+            si = line.find(b';')
+            locations.append(line[:si])
     return locations
 
-def write_new_measurements(locations, n = 1_000_000_000, batch_size=10_000):
+def generate_and_write_sample_data(args) -> None:
+    locations, k = args[0], args[1]
+    out_str = ''
+    sampled_locations = random.choices(locations,k=k)
 
-    n_batches = n//batch_size
-    with open('measurements.csv', 'wb') as f:
+    for station in sampled_locations:
+        out_str += f"{station};{random.uniform(COLDEST_TEMP, HOTTEST_TEMP):.1f}\n" #for station in sampled_locations
 
-        for bidx in range(n_batches):
-            if bidx%10 == 0:
-                print(f"Batch {bidx} / {n_batches}")
-            b = random.choices(locations, k=batch_size)
-            z = [bytes(f"{station};{random.uniform(COLDEST_TEMP, HOTTEST_TEMP):.1f}\n", encoding='utf-8') for station in b]
-            # prepped_deviated_batch = '\n'.join(z) # :.1f should quicker than round on a large scale, because round utilizes mathematical operation
-            f.writelines(z)
-            # f.write(prepped_deviated_batch)
-        
+    with open('measurements.csv', 'a',encoding='utf-8') as f:
+        f.write(out_str)
+
+def main():
+    locations = get_locations()
+    # Write a million datapoints per function call. 
+    N = 1_000_000_000
+    k = 1_000_000
+
+
+    import os
+    pool = Pool(processes=os.cpu_count())
+    pool.map(generate_and_write_sample_data, [(locations,k) for _ in range(N//k)])
+    # generate_and_write_sample_data()
+
+
+
+    # write_new_measurements(locations)
+
 
     
 
 if __name__ == '__main__':
+    # cProfile.run('main()')
+
     main()
