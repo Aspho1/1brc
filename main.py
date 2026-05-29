468a342951bb9c8c0990789dabdeeed27dd3c324
Aspho1 <thl3141@gmail.com>
2026-05-29 14:35:06 -0500

    down to 50 seconds by using imap_unordered and partial dicts for subprocesses.
    Signed-off-by: Aspho1 <thl3141@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from multiprocessing import Pool
 # from multiprocessing
 import sys
-import threading
 import time
 import cProfile
 # from typing import 
@@ -20,28 +19,39 @@ def _process_chunk(args):
     with open(filename, 'rb') as f:
         f.seek(start)
         for line in f.read(end - start).split(b'\n'):
+            if not line:
+                continue
             si = line.find(b';')
-            loc, temp = line[:si], float(line[si+1:])
+            loc = line[:si]
+            temp = float(line[si+1:])
             if loc in local:
                 v = local[loc]
-                if temp < v[0]: v[0] = temp
-                if temp > v[1]: v[1] = temp
+                if temp < v[0]:
+                    v[0] = temp
+                if temp > v[1]:
+                    v[1] = temp
                 v[2] += temp
                 v[3] += 1
             else:
                 local[loc] = [temp, temp, temp, 1]
     return local
 
-
-def main(cpu_count = os.cpu_count(), target_filename = 'measurements.csv', peek_dist = 25):
+def main(cpu_count = os.cpu_count(), target_filename = 'measurements.csv', peek_dist = 25 ):
 
     file_size = os.path.getsize(target_filename)
-    step = file_size // cpu_count
+    step = max(2**22, file_size // (cpu_count * 2))
+    n_steps = file_size // step
+    
+    print(f'USING {n_steps} CHUNKS and {cpu_count} CPUs')
+    # step = file_size // cpu_count
+
+    # print(step)
+    # return
 
     chunks = []
     x=0
     with open(target_filename, 'r+b') as f:
-        for i in range(cpu_count):
+        for i in range(n_steps):
             start = x
             end = min(x+step,file_size)
 
@@ -52,58 +62,34 @@ def main(cpu_count = os.cpu_count(), target_filename = 'measurements.csv', peek_
             x=end
 
     with Pool(cpu_count) as pool:
-        results = pool.map(_process_chunk, chunks)
-
-
-    for partial in results:
-        for loc, v in partial.items():
-            if loc in d:
-                d[loc][0] = min(d[loc][0], v[0])
-                d[loc][1] = max(d[loc][1], v[1])
-                d[loc][2] += v[2]
-                d[loc][3] += v[3]
-            else:
-                d[loc] = v
-    return d
-
-
-
-        #     semicolon_idx = line.find(b";")
-        #     loc, temp = line[:semicolon_idx], float(line[semicolon_idx+1:-1])
-        #     if loc not in d:
-        #         d[loc] = [temp, temp, temp, 1]
-        #     else:
-        #         if temp < d[loc][0]:
-        #             d[loc][0] = temp
-        #         if temp > d[loc][1]:
-        #             d[loc][1] = temp
-        #         d[loc][2] += temp
-        #         d[loc][3] += 1
-
-        #     z+=1
-            # if z % 10000 == 0: 
-            #     # print(loc, d[loc])
-            #     if z > 1000000:
-            #         return
-
+        for partial in pool.imap_unordered(_process_chunk, chunks):
+            for loc, v in partial.items():
+                if loc in d:
+                    d[loc][0] = min(d[loc][0], v[0])
+                    d[loc][1] = max(d[loc][1], v[1])
+                    d[loc][2] += v[2]
+                    d[loc][3] += v[3]
+                else:
+                    d[loc] = v
 
 def printd():
-
     print("{",end="")
 
-    while len(d) > 1:
+    while len(d) >= 2:
         (k, vs) = d.popitem()
         k = k.decode('utf-8')
         print(f'{k}={vs[0]:.1f}/{vs[2]/vs[3]:.1f}/{vs[1]:.1f}, ',end='')
     k, vs = d.popitem()
     k = k.decode('utf-8')
     print(f'{k}={vs[0]:.1f}/{vs[2]/vs[3]:.1f}/{vs[1]:.1f}',end='')
-    print("}",end="")
+    print("}")
 
 
 
 
-
+# 249.645 seconds on 5 CPUs
+# 50.493 seconds on 32 CPUs (128 chunks)
+# 53.776 seconds on 64 CPUs (128 chunks)
 if __name__ == "__main__":
 
     ob = cProfile.Profile()
@@ -111,12 +97,12 @@ if __name__ == "__main__":
     # define_chunks()
 
     ob.enable()
-    d = main()
+    main()
     ob.disable()
-    printd(d)
+    printd()
 
     sec = io.StringIO()
-    sortby = SortKey.CUMULATIVE
+    sortby = SortKey.TIME
     ps = pstats.Stats(ob, stream=sec).sort_stats(sortby)
     ps.print_stats()
 
@@ -127,3 +113,4 @@ if __name__ == "__main__":
     # end = time.time_ns()
 
     # print(f'Execution took {(end-start)/1e9} seconds')
+
